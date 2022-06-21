b7fe2267a547ea58e083d7864809e274663a7a3f
lucas-fernandes <luks.faf@gmail.com>
2022-06-21 07:22:20 -0300

    update

--- src/models/db.sql
+++ src/models/db.sql
@@ -1,25 +1,32 @@
+/*  -- Script BD --  */
 CREATE DATABASE dblogin;
 
-use dblogin;
+USE dblogin;
 
 CREATE TABLE users(
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR (100) NOT NULL,
     sobrenome VARCHAR (100) NOT NULL,
-    nascimento INTEGER NOT NULL,
+    nascimento DATE NOT NULL,
     email VARCHAR (100) NOT NULL,
-    telefone INTEGER NOT NULL,
-    cars_id VARCHAR(100) NOT NULL,
-    user VARCHAR (100) NOT NULL,
+    telefone BIGINT(14) NOT NULL,
+    nome_car VARCHAR(50) NOT NULL,
+    user VARCHAR (50) NOT NULL,
     pass INTEGER NOT NULL
 
 );
 
 CREATE TABLE cars(
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL PRIMARY KEY,
     modelo VARCHAR (100) NOT NULL,
-    placa VARCHAR (100) NOT NULL,
-    cor VARCHAR (100) NOT NULL,
-    fabricacao INTEGER NOT NULL
+    placa VARCHAR (10) NOT NULL,
+    cor VARCHAR (10) NOT NULL,
+    fabricacao DATE NOT NULL
 
 );
+
+ALTER TABLE `users` ADD CONSTRAINT `fk_car` FOREIGN KEY ( `cars_id` ) REFERENCES `cidade` ( `codcidade` ) ;
+
+/* 
+    ALTER TABLE users ADD FOREIGN KEY(cars_id) REFERENCES cars(id); 
+*/
